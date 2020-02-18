84edd6e5225c2ee51df8445b5b6514d9ba53baee
Miles Olson <mpolson64@gmail.com>
2020-02-18 13:24:17 -0600

    Files generated by PkgTemplates

---
+++ src/ClutteredEnvPathOpt.jl
@@ -0,0 +1,5 @@
+module ClutteredEnvPathOpt
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ClutteredEnvPathOpt
+using Test
+
+@testset "ClutteredEnvPathOpt.jl" begin
+    # Write your own tests here.
+end
