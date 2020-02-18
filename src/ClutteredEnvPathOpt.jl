070d220234c65b6500cd5d5e1ed59301a32c18b2
Miles Olson <mpolson64@gmail.com>
2020-02-18 13:55:03 -0600

    Simple bfs returning parent mapping

--- src/ClutteredEnvPathOpt.jl
+++ src/ClutteredEnvPathOpt.jl
@@ -1,5 +1,22 @@
 module ClutteredEnvPathOpt
 
-greet() = print("Hello World!")
+function bfs(graph::Dict{T, Set{T}}, root::T)::Dict{T, T} where {T}
+    parents = Dict{T, T}()
+    visited = Set(root)
+    queue = [root]
+
+    while !isempty(queue)
+        node = pop!(queue)
+        for neighbor in graph[node]
+            if !in(neighbor, visited)
+                push!(visited, neighbor)
+                pushfirst!(queue, neighbor)
+                parent[neighbor] = node
+            end
+        end
+    end
+
+    return parents
+end
 
 end # module
