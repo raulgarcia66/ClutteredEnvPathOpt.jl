20ac453702191620faf7f72ca2e1970c9b929412
Miles Olson <mpolson64@gmail.com>
2020-02-19 16:44:39 -0600

    Find fundamental cycle

--- src/ClutteredEnvPathOpt.jl
+++ src/ClutteredEnvPathOpt.jl
@@ -21,4 +21,40 @@ end
 
 bfs(graph::Dict{T, Set{T}}) where {T} = bfs(graph, collect(keys(graph))[1])
 
+function edges(graph::Dict{T, Set{T}})::Set{Set{T}} where {T}
+    edges_per_node = map(pair -> map(dest -> Set([pair.first, dest]), collect(pair.second)), collect(graph))
+    return Set(reduce(union, edges_per_node))
+end
+
+function tree_edges(parents::Dict{T, T})::Set{Set{T}} where {T}
+    return Set(map(pair -> Set(pair), collect(parents)))
+end
+
+function find_fundamental_cycle(parents::Dict{T, T}, non_tree_edge::Set{T})::Set{T} where {T}
+    ordered_edge = collect(non_tree_edge)
+    left_node = ordered_edge[1]
+    right_node = ordered_edge[2]
+
+    left_seen = [left_node]
+    right_seen = [right_node]
+
+    while true
+        left_node = get(parents, left_node, nothing)
+        right_node = get(parents, right_node, nothing)
+
+        if isnothing(left_node) && isnothing(right_node)
+            return Set()
+        elseif left_node == right_node
+            return Set(union(left_seen, right_seen, left_node))
+        elseif !isnothing(left_node) && in(left_node, right_seen)
+            return Set(union(left_seen, right_seen[1:(findall(x -> x == left_node, right_seen)[1])]))
+        elseif !isnothing(right_node) &&  in(right_node, left_seen)
+            return Set(union(right_seen, left_seen[1:(findall(x -> x == right_node, left_seen)[1])]))
+        else
+            if !isnothing(left_node) push!(left_seen, left_node) end
+            if !isnothing(right_node) push!(right_seen, right_node) end
+        end
+    end
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,27 +1,27 @@
 using ClutteredEnvPathOpt
 using Test
 
+small_ring = Dict(
+    1 => Set([2, 5]),
+    2 => Set([1, 3]),
+    3 => Set([2, 4]),
+    4 => Set([3, 5]),
+    5 => Set([4, 1]),
+)
+
+cross = Dict(
+    1 => Set([2]),
+    2 => Set([1, 3, 4, 5]),
+    3 => Set([2]),
+    4 => Set([2]),
+    5 => Set([2]),
+)
+
 @testset "ClutteredEnvPathOpt.jl" begin
     # Write your own tests here.
 end
 
 @testset "bfs tests" begin
-    small_ring = Dict(
-        1 => Set([2, 5]),
-        2 => Set([1, 3]),
-        3 => Set([2, 4]),
-        4 => Set([3, 5]),
-        5 => Set([4, 1]),
-    )
-
-    cross = Dict(
-        1 => Set([2]),
-        2 => Set([1, 3, 4, 5]),
-        3 => Set([2]),
-        4 => Set([2]),
-        5 => Set([2]),
-    )
-
     @test ClutteredEnvPathOpt.bfs(small_ring, 1) == Dict(
         2 => 1,
         3 => 2,
@@ -42,4 +42,30 @@ end
         4 => 2,
         5 => 2,
     )
+end
+
+@testset "edge counting tests" begin
+    @test ClutteredEnvPathOpt.edges(small_ring) == Set([
+        Set([1, 2]),
+        Set([2, 3]),
+        Set([3, 4]),
+        Set([4, 5]),
+        Set([5, 1]),
+    ])
+    @test ClutteredEnvPathOpt.edges(cross) == Set([
+        Set([1, 2]),
+        Set([2, 3]),
+        Set([2, 4]),
+        Set([2, 5]),
+    ])
+    @test ClutteredEnvPathOpt.tree_edges(Dict(2 => 1, 3 => 2, 4 => 5, 5 => 1)) == Set([
+        Set([1, 2]),
+        Set([2, 3]),
+        Set([4, 5]),
+        Set([5, 1]),
+    ])
+end
+
+@testset "fundamental cycle identifying tests" begin
+    @test ClutteredEnvPathOpt.find_fundamental_cycle(ClutteredEnvPathOpt.bfs(small_ring, 1), Set([3, 4])) == keys(small_ring)
 end
