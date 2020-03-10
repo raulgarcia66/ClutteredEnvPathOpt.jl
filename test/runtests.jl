6553df5b97868d409452dbe67f9b5e603dcd6cd3
Miles Olson <mpolson64@gmail.com>
2020-03-10 12:29:30 -0500

    Redid tests on del graphs

--- test/runtests.jl
+++ test/runtests.jl
@@ -68,42 +68,63 @@ GENERATORS = [ClutteredEnvPathOpt.LightGraphs.cycle_graph, ClutteredEnvPathOpt.L
     end
 end
 
-# @testset "separator tests random graphs" begin
-#     filenames = [
-#         "a280",
-#         # "bier127",
-#         # "ch130",
-#         # "ch150",
-#         # "d198",
-#     ]
-#     for filename in filenames
-#         open(("test/delaunay-graphs/$filename.tsp")) do file
-#             lines = readlines(file)
-#             graph = @pipe match(r"\d+", lines[1]) |> parse(Int, _.match) |> ClutteredEnvPathOpt.LightGraphs.SimpleGraph
+@testset "separator tests provided graphs" begin
+    filenames = [
+        "a280",
+        "bier127",
+        "ch130",
+        "ch150",
+        "d198",
+        "d1291",
+        "d1655",
+        "d2103",
+        "d493",
+        "d657",
+        "eil101",
+        "eil51",
+        "eil76",
+        "fl1400",
+        "fl1577",
+        "fl417",
+        "gil262",
+        "kroA100",
+        "kroA150",
+        "kroA200",
+        "kroB100",
+        "kroB150",
+        "kroB200",
+        "kroC100",
+        "kroE100"
+    ]
+    for filename in filenames
+        open(("delaunay-graphs/$filename.tsp.del")) do file
+            lines = readlines(file)
+            graph = @ClutteredEnvPathOpt.pipe match(r"\d+", lines[1]) |> parse(Int, _.match) |> ClutteredEnvPathOpt.LightGraphs.SimpleGraph
     
-#             for line in lines[7:end - 1]
-#                 edge = map(rm -> parse(Int, rm.match), collect(eachmatch(r"\d+", line)))
-#                 ClutteredEnvPathOpt.LightGraphs.add_edge!(graph, edge[2], edge[3]);
-#             end
-    
-#             lg = LabeledGraph(graph)
-#             (separator, a, b) = find_separator_fcs(lg, 1)
+            for line in lines[2:end]
+                edge = map(rm -> parse(Int, rm.match), collect(eachmatch(r"\d+", line)))
+                ClutteredEnvPathOpt.LightGraphs.add_edge!(graph, edge[1] + 1, edge[2] + 1); # n + 1 bc graph is 0-indexed
+            end
+            lg = LabeledGraph(graph)
+            (separator, a, b) = find_separator_fcs(lg, 1)
 
-#             is_valid = true
-#             for source in a
-#                 break_early = false
-#                 for destination in b
-#                     is_path = ClutteredEnvPathOpt.LightGraphs.has_path(lg.graph, source, destination, exclude_vertices=collect(separator))
-#                     is_valid = is_valid && !is_path
-#                     if !is_valid
-#                         break_early = true
-#                         break
-#                     end
-#                 end
+            is_valid = true
+            for source in a
+                break_early = false
+                for destination in b
+                    is_path = ClutteredEnvPathOpt.LightGraphs.has_path(lg.graph, source, destination, exclude_vertices=collect(separator))
+                    is_valid = is_valid && !is_path
+                    if !is_valid
+                        break_early = true
+                        break
+                    end
+                end
 
-#                 if break_early break end
-#             end
-#             @test is_valid
-#         end
-#     end
-# end
+                if break_early break end
+            end
+            @test is_valid
+
+            @test all(map(vertex -> in(vertex, union(separator, a, b)), collect(keys(lg.labels))))
+        end
+    end
+end
