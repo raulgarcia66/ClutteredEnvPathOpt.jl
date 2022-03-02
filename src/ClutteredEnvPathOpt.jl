351a78e9b3ba8a1075900effa147a8194652582f
Raul Garcia <rauljgarcia66@gmail.com>
2022-03-01 22:08:10 -0600

    To do note

--- src/ClutteredEnvPathOpt.jl
+++ src/ClutteredEnvPathOpt.jl
@@ -3,6 +3,7 @@ using Pipe
 import LightGraphs
 
 export LabeledGraph, find_feg_separator_lt, find_separator_lt, find_separator_fcs, find_separator_fcs_best, pp_expell, find_biclique_cover, find_feg_separator_lt_best
+# export solve_deits, plot_steps, plot_circles, plot_points, all other useful functions
 include("types.jl")
 include("separator.jl")
 include("biclique.jl")
