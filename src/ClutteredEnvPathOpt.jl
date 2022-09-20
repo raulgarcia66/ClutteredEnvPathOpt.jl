fc070e03374e5430ed0b37db00db82dc4566e761
Raul Garcia <rauljgarcia66@gmail.com>
2022-09-20 13:53:05 -0500

    Exported more

--- src/ClutteredEnvPathOpt.jl
+++ src/ClutteredEnvPathOpt.jl
@@ -4,7 +4,7 @@ import LightGraphs
 
 export LabeledGraph, find_feg_separator_lt, find_separator_lt, find_separator_fcs, find_separator_fcs_best, pp_expell, find_biclique_cover, find_feg_separator_lt_best
 export find_biclique_cover
-export solve_steps, plot_steps, plot_circles
+export solve_steps, plot_steps, plot_circles, gen_obstacle, gen_obstacle_from_file, gen_field_random, gen_field
 include("types.jl")
 include("separator.jl")
 include("biclique.jl")
