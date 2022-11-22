9ec54e16c8d5861a6adfe6cae2a4c56b4c9db29f
Raul Garcia <rauljgarcia66@gmail.com>
2022-11-21 19:08:26 -0600

    Added vertex partition coloring; Upd plot settings

--- src/plotting.jl
+++ src/plotting.jl
@@ -74,6 +74,34 @@ function plot_edges(lg::LabeledGraph{T}, points::Vector{Pair{Rational{Int}}}; pl
 end
 
 """
+    plot_edges(lg, points; plot_name, col, new_plot)
+
+Given a LabeledGraph, plots its nodes and edges in the unit square by partition.
+"""
+function plot_edges(lg::LabeledGraph{T}, points::Vector{Pair{Rational{Int}}}, partition::Tuple{Set{Int},Set{Int},Set{Int}}; plot_name::String="Edges", col::String="colorful", new_plot::Bool=true, vertices::Dict{T,T}=Dict{T,T}(), with_labels=true) where {T}
+    if new_plot
+        Plots.plot()
+    end
+
+    # Need to map nodes to the point they refer to in "points"
+    rev = ClutteredEnvPathOpt._reverse_labels(lg.labels)
+    for edge in LightGraphs.edges(lg.graph)
+        if col == "colorful"
+            Plots.plot!([points[rev[edge.src]].first, points[rev[edge.dst]].first], [points[rev[edge.src]].second, points[rev[edge.dst]].second],linewidth=2)
+            # display(Plots.plot!(title="Edge ($(rev[edge.src]), $(rev[edge.dst]))"))
+        else
+            Plots.plot!([points[rev[edge.src]].first, points[rev[edge.dst]].first], [points[rev[edge.src]].second, points[rev[edge.dst]].second], color=col,linewidth=2)
+            # display(Plots.plot!(title="Edge ($(rev[edge.src]), $(rev[edge.dst]))"))
+        end
+    end
+
+    # ClutteredEnvPathOpt.plot_intersections(obstacles, vertices=vertices)
+    ClutteredEnvPathOpt.plot_points(points, partition, vertices=vertices, with_labels=with_labels)
+
+    display(Plots.plot!(title=plot_name, xlims=(-0.05,1.05), ylims=(-0.05,1.05), legend=false))
+end
+
+"""
     plot_biclique_cover(lg, points, cover; with_all)
 
 Given a LabeledGraph and a biclique cover, plot each biclique.
@@ -93,7 +121,7 @@ function plot_biclique_cover(lg::LabeledGraph{T}, points::Vector{Pair{Rational{I
     #     LightGraphs.add_edge!(temp_graph, lg.labels[edge.first], lg.labels[edge.second])
     # end
 
-    colors = [:firebrick1, :dodgerblue, :limegreen]
+    # colors = [:firebrick1, :dodgerblue, :limegreen]
 
     # Plot biclique
     for (j,biclique_edges) in enumerate(BC_edges)
@@ -115,7 +143,8 @@ function plot_biclique_cover(lg::LabeledGraph{T}, points::Vector{Pair{Rational{I
         end
 
         for edge in biclique_edges
-            Plots.plot!([points[rev[edge.first]].first, points[rev[edge.second]].first], [points[rev[edge.first]].second, points[rev[edge.second]].second], linewidth=2, color=colors[(j % 3) + 1])
+            # Plots.plot!([points[rev[edge.first]].first, points[rev[edge.second]].first], [points[rev[edge.first]].second, points[rev[edge.second]].second], linewidth=2, color=colors[(j % 3) + 1])
+            Plots.plot!([points[rev[edge.first]].first, points[rev[edge.second]].first], [points[rev[edge.first]].second, points[rev[edge.second]].second], linewidth=2, color=:green3)
             # display(Plots.plot!(title="Edge ($(rev[edge.first]), $(rev[edge.second]))"))
         end
 
@@ -124,18 +153,22 @@ function plot_biclique_cover(lg::LabeledGraph{T}, points::Vector{Pair{Rational{I
         y = map(point -> point.second, points)
         for i = 1:length(points)
             if i in cover_vec[j].first
-                Plots.scatter!([x[i]],[y[i]], color="red", markersize = 7) #, series_annotations=([Plots.text(string(i), :right, 8, "courier")]))
+                # Plots.scatter!([x[i]],[y[i]], color="red", markersize = 7) #, series_annotations=([Plots.text(string(i), :right, 8, "courier")]))
+                Plots.scatter!([x[i]],[y[i]], color="red", markersize = 7, series_annotations=([Plots.text(string(i), :top, 15)]))
             elseif i in cover_vec[j].second
-                Plots.scatter!([x[i]],[y[i]], color="blue", markersize = 7) #, series_annotations=([Plots.text(string(i), :right, 8, "courier")]))
+                # Plots.scatter!([x[i]],[y[i]], color="blue", markersize = 7) #, series_annotations=([Plots.text(string(i), :right, 8, "courier")]))
+                Plots.scatter!([x[i]],[y[i]], color="blue", markersize = 7, series_annotations=([Plots.text(string(i), :top, 15)]))
             else
-                Plots.scatter!([x[i]],[y[i]], color="grey35", markersize = 7) #, series_annotations=([Plots.text(string(i), :right, 8, "courier")]))
+                # Plots.scatter!([x[i]],[y[i]], color="grey35", markersize = 7) #, series_annotations=([Plots.text(string(i), :right, 8, "courier")]))
+                Plots.scatter!([x[i]],[y[i]], color="grey35", markersize = 7, series_annotations=([Plots.text(string(i), :top, 15)]))
             end
         end
 
         display(Plots.plot!(title="Biclique $j"))
+        # display(Plots.plot!())
         if save_plots
             # savefig("$name $j.pdf")
-            png("$name $j.pdf")
+            png("$name $j")
         end
     end
 end
@@ -276,7 +309,7 @@ function plot_points(points::Vector{Pair{Rational{Int}}}; vertices::Dict{Int,Int
         x = map(point -> point.first, points_filtered)
         y = map(point -> point.second, points_filtered)
         if with_labels
-            Plots.scatter!(x,y, color="red", markersize=7, series_annotations=([Plots.text(string(x), :right, 10, "courier") for x in keys(vertices)]))
+            Plots.scatter!(x,y, color="red", markersize=7, series_annotations=([Plots.text(string(x), :top, 15) for x in keys(vertices)]))
         else
             Plots.scatter!(x,y, color="red", markersize=7)
         end
@@ -284,12 +317,85 @@ function plot_points(points::Vector{Pair{Rational{Int}}}; vertices::Dict{Int,Int
         x = map(point -> point.first, points)
         y = map(point -> point.second, points)
         if with_labels
-            Plots.scatter!(x,y, color="red", markersize=7, series_annotations=([Plots.text(string(x), :right, 10, "courier") for x in 1:length(points)]))
+            Plots.scatter!(x,y, color="red", markersize=7, series_annotations=([Plots.text(string(x), :top, 15) for x in 1:length(points)]))
         else
             Plots.scatter!(x,y, color="red", markersize=7)
         end
     end
 
+end
+
+"""
+    plot_points(points; vertices)
+
+Plots and labels points given by partition. Optional argument to plot a subset of vertices.
+"""
+function plot_points(points::Vector{Pair{Rational{Int}}}, partition::Tuple{Set{Int},Set{Int},Set{Int}}; vertices::Dict{Int,Int}=Dict{Int,Int}(), with_labels=true)
+    
+    Plots.plot!(legend=false)
+    A,B,C = partition
+
+    if !isempty(vertices)
+        # points_filtered = []
+        points_filtered_A = []
+        points_filtered_B = []
+        points_filtered_C = []
+        v_filtered_A = []
+        v_filtered_B = []
+        v_filtered_C = []
+        for v in keys(vertices)
+            # push!(points_filtered, points[v])
+            if v in A
+                push!(points_filtered_A, points[v])
+                push!(v_filtered_A, v)
+            elseif v in B
+                push!(points_filtered_B, points[v])
+                push!(v_filtered_B, v)
+            else
+                push!(points_filtered_C, points[v])
+                push!(v_filtered_C, v)
+            end
+        end
+        # x = map(point -> point.first, points_filtered)
+        # y = map(point -> point.second, points_filtered)
+        x_A = map(point -> point.first, points_filtered_A)
+        y_A = map(point -> point.second, points_filtered_A)
+        x_B = map(point -> point.first, points_filtered_B)
+        y_B = map(point -> point.second, points_filtered_B)
+        x_C = map(point -> point.first, points_filtered_C)
+        y_C = map(point -> point.second, points_filtered_C)
+        if with_labels
+            Plots.scatter!(x_A,y_A, color="red", markersize=7, series_annotations=([Plots.text(string(x), :top, 15) for x in v_filtered_A]))
+            Plots.scatter!(x_B,y_B, color="blue", markersize=7, series_annotations=([Plots.text(string(x), :top, 15) for x in v_filtered_B]))
+            Plots.scatter!(x_C,y_C, color="green", markersize=7, series_annotations=([Plots.text(string(x), :top, 15) for x in v_filtered_C]))
+        else
+            Plots.scatter!(x_A,y_A, color="red", markersize=7)
+            Plots.scatter!(x_B,y_B, color="blue", markersize=7)
+            Plots.scatter!(x_C,y_C, color="green", markersize=7)
+        end
+    else
+        # x = map(point -> point.first, points)
+        # y = map(point -> point.second, points)
+        AA = collect(A)
+        BB = collect(B)
+        CC = collect(C)
+        x_A = [points[v].first for v in AA]
+        y_A = [points[v].second for v in AA]
+        x_B = [points[v].first for v in BB]
+        y_B = [points[v].second for v in BB]
+        x_C = [points[v].first for v in CC]
+        y_C = [points[v].second for v in CC]
+
+        if with_labels
+            Plots.scatter!(x_A,y_A, color="red", markersize=7, series_annotations=([Plots.text(string(x), :top, 15) for x in AA]))
+            Plots.scatter!(x_B,y_B, color="blue", markersize=7, series_annotations=([Plots.text(string(x), :top, 15) for x in BB]))
+            Plots.scatter!(x_C,y_C, color="green", markersize=7, series_annotations=([Plots.text(string(x), :top, 15) for x in CC]))
+        else
+            Plots.scatter!(x_A,y_A, color="red", markersize=7)
+            Plots.scatter!(x_B,y_B, color="blue", markersize=7)
+            Plots.scatter!(x_C,y_C, color="green", markersize=7)
+        end
+    end
 end
 
 """
